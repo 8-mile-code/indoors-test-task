f6197ca85697e4498555b2592584ee8629ad8105
Danil Bogatov <danil.boghatov17@yandex.ru>
2026-05-18 13:29:20 +0300

    Improve frontend layout and navigation

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
